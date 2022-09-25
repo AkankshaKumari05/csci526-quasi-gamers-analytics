c7c97c0c5e791e2078c66938427d2449f7da4b5b
AkankshaKumari05 <akanksha5996@gmail.com>
2022-09-24 20:35:59 -0700

    Update service.py

--- service.py
+++ service.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_pymongo import pymongo
 
-
 CONNECTION_STRING = "mongodb+srv://root:abcd1234@quasi-gamers.ayeicnx.mongodb.net/?retryWrites=true&w=majority"
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('QuasiGamers')
